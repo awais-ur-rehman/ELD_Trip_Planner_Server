773af045d3192ccff60ff554377dce6f8ad2c670
awais-ur-rehman <awaisjarral37@gmail.com>
2026-05-02 22:15:31 +0500

    fixed: render SSL termination issue

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,8 @@ from .base import *  # noqa: F401, F403
 
 DEBUG = False
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 SECURE_SSL_REDIRECT            = True
 SECURE_HSTS_SECONDS            = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
